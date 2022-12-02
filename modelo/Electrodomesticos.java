f29d3b604aa8102082fc5a047e260994647abb24
Javier David Nino Ruiz <103191427+JdavidN@users.noreply.github.com>
2022-12-02 17:22:50 -0500

    Add files via upload

---
+++ modelo/Electrodomesticos.java
@@ -0,0 +1,64 @@
+package modelo;
+
+public class Electrodomesticos 
+{
+    private String televisor;
+    private String nevera;
+    private String lavadora;
+    private String microondas;
+    private String aspiradora;
+
+    public String getAspiradora() 
+    {
+        return aspiradora;
+    }
+
+    public String getLavadora() 
+    {
+        return lavadora;
+    }
+
+    public String getMicroondas() 
+    {
+        return microondas;
+    }
+
+    public String getNevera() 
+    {
+        return nevera;
+    }
+
+    public String getTelevisor() 
+    {
+        return televisor;
+    }
+
+    public void setAspiradora(String aspiradora) 
+    {
+        this.aspiradora = aspiradora;
+    }
+
+    public void setLavadora(String lavadora) 
+    {
+        this.lavadora = lavadora;
+    }
+
+    public void setMicroondas(String microondas) 
+    {
+        this.microondas = microondas;
+    }
+
+    public void setNevera(String nevera) 
+    {
+        this.nevera = nevera;
+    }
+
+    public void setTelevisor(String televisor) 
+    {
+        this.televisor = televisor;
+    }
+    
+    
+
+
+}
---
+++ modelo/FigurasGeometricas.java
@@ -0,0 +1,58 @@
+package modelo;
+public class FigurasGeometricas
+
+{
+    private String circulo;
+    private String cuadrado;
+    private String triangulo;
+    private String rectangulo;
+    private String rombo;
+
+    public String getCirculo()
+    {
+        return circulo;
+    }
+
+    public String getCuadrado() 
+    {
+        return cuadrado;
+    }
+
+    public String getRectangulo() 
+    {
+        return rectangulo;
+    }
+
+    public String getRombo() 
+    {
+        return rombo;
+    }
+    public String getTriangulo() 
+    {
+        return triangulo;
+    }
+
+    public void setCirculo(String circulo)
+    {
+        this.circulo = circulo;
+    }
+
+    public void setCuadrado(String cuadrado) 
+    {
+        this.cuadrado = cuadrado;
+    }
+
+    public void setRectangulo(String rectangulo) 
+    {
+        this.rectangulo = rectangulo;
+    }
+
+    public void setRombo(String rombo) 
+    {
+        this.rombo = rombo;
+    }
+    public void setTriangulo(String triangulo) 
+    {
+        this.triangulo = triangulo;
+    }
+}
