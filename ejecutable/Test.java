3ff8cef37015fbfdae903f70455d39ded37270be
Javier David Nino Ruiz <103191427+JdavidN@users.noreply.github.com>
2022-12-03 03:09:43 -0500

    Add files via upload

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -2,13 +2,17 @@ package ejecutable;
 
 import controlador.Controlador;
 import vista.VentanaPrincipal;
+import modelo.Usuario;
 
 public class Test
 {
     public static void main (String[] args)
     {
         VentanaPrincipal venPrin = new VentanaPrincipal();
-        Controlador miControlador = new Controlador(venPrin);
+
+        Usuario model = new Usuario("", 0);
+        Controlador miControlador = new Controlador(venPrin, model);
+
      
         
       
